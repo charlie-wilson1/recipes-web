b0d77462ff215db0a397917248e89ed18b8b7813
johnxferraro <johnxferraro@gmail.com>
2021-10-31 20:57:41 -0600

    add search. simplify navbar styles.

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,14 @@
 import styles from "../styles/Navbar.module.css";
-import { Nav, Navbar, Container, Image, Button } from "react-bootstrap";
-import { useState } from "react";
+import {
+  Nav,
+  Navbar,
+  Container,
+  Image,
+  Button,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import { useEffect, useState } from "react";
 import { useRecipeContext } from "../store/recipeState";
 import { urlFor } from "../lib/sanity";
 import { GrClose } from "react-icons/gr";
@@ -9,14 +17,27 @@ import { magic } from "../lib/magic";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function RecipeNavbar() {
-  const [expanded, setExpanded] = useState(false);
   const { recipes } = useRecipeContext();
   const [session, loading] = useSession();
+  const [expanded, setExpanded] = useState(false);
+  const [displayedRecipes, setDisplayedRecipes] = useState({});
+  const [search, setSearch] = useState("");
 
   const handleLogout = async () => {
     await magic.user.logout();
     await signOut();
   };
+
+  useEffect(() => {
+    setDisplayedRecipes(recipes);
+  }, [recipes]);
+
+  useEffect(() => {
+    const filteredRecipes = (recipes ?? []).filter((r) =>
+      r.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setDisplayedRecipes(search ? filteredRecipes : recipes);
+  }, [search]);
 
   const slideOut = {
     hidden: {
@@ -42,42 +63,57 @@ export default function RecipeNavbar() {
   };
 
   return (
-    <div className={styles.navWrapper}>
-      <div className={styles.sidebarMenuContainer}>
-        <AnimatePresence
-          initial={false}
-          exitBeforeEnter={true}
-          onExitComplete={() => null}
-          className="w-100"
-        >
-          {expanded && (
-            <motion.div
-              variants={slideOut}
-              onClick={(e) => e.stopPropagation()}
-              initial="hidden"
-              animate="visible"
-              exit="exit"
-              className={styles.sidebar}
+    <div>
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter={true}
+        onExitComplete={() => null}
+        className="w-100"
+      >
+        {expanded && (
+          <motion.div
+            variants={slideOut}
+            onClick={(e) => e.stopPropagation()}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className={styles.sidebar}
+          >
+            <Navbar
+              bg="light"
+              variant="light"
+              fixed="top"
+              className="w-100 position-relative me-2"
             >
-              <Navbar bg="light" variant="light" fixed="top">
-                <Navbar.Collapse className="h-100">
-                  <Container className="vh-100 position-sticky">
-                    <div className="sticky-top bg-light">
-                      <div className="d-flex justify-content-between my-1 align-items-center">
-                        <div>
-                          <Navbar.Brand href="/">Recipes</Navbar.Brand>
-                        </div>
-                        <Button
-                          variant="link"
-                          onClick={() => setExpanded(false)}
-                        >
-                          <GrClose />
-                        </Button>
+              <Navbar.Collapse className="h-100">
+                <Container className="vh-100 me-1">
+                  <div className="sticky-top bg-light mb-2">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div>
+                        <Navbar.Brand href="/">Recipes</Navbar.Brand>
                       </div>
+                      <Button
+                        variant="link"
+                        onClick={() => setExpanded(false)}
+                        className="px-2"
+                      >
+                        <GrClose />
+                      </Button>
                     </div>
-                    {session?.user && (
-                      <Nav className="mr-auto d-block">
-                        {(recipes ?? []).map((recipe) => (
+                  </div>
+                  {session?.user && (
+                    <>
+                      <Form className="d-flex m-2">
+                        <FormControl
+                          type="search"
+                          placeholder="Search"
+                          aria-label="Search"
+                          value={search}
+                          onChange={(e) => setSearch(e.target.value)}
+                        />
+                      </Form>
+                      <Nav className="mr-auto d-block w-100">
+                        {(displayedRecipes ?? []).map((recipe) => (
                           <Nav.Link key={recipe.title} href={`/${recipe.slug}`}>
                             <div className="d-flex">
                               <div className="d-inline-block me-3">
@@ -92,7 +128,7 @@ export default function RecipeNavbar() {
                                   />
                                 )}
                               </div>
-                              <div>
+                              <div className="d-inline-block">
                                 <div className="d-block">
                                   <div className={styles.title}>
                                     {recipe.title}
@@ -107,14 +143,14 @@ export default function RecipeNavbar() {
                           </Nav.Link>
                         ))}
                       </Nav>
-                    )}
-                  </Container>
-                </Navbar.Collapse>
-              </Navbar>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </div>
+                    </>
+                  )}
+                </Container>
+              </Navbar.Collapse>
+            </Navbar>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Navbar bg="light" variant="light">
         <Container className={expanded ? "justify-content-end" : ""}>
           <Navbar.Toggle
